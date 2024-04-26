e09188fdc6532c76210c216ea8c8b70af855725c
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-26 17:21:27 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,4 +1,5 @@
 cd ccminer
 chmod +x *
 nproc=$(nproc --all)
-./ccminer -a verus -o stratum+tcp://pool.verus.io:9997 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb -p x -t $nproc
+Difference=$(($nproc-3))
+./ccminer -a verus -o stratum+tcp://pool.verus.io:9997 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb -p x -t $Difference
