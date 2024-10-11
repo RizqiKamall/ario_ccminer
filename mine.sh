d7c2994bc3d036e6872c356f1c0bccf03916980f
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-10-11 08:29:34 +0000

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 chmod +x *
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.Rig003 -p d=16384S
+./hellminer -c stratum+ssl://na.luckpool.net:3958 -u REPNzMPtM7seJy5xngt5VWKXMsEi6Ejezb.AkuIkan_X_DinoKuning -p d=16384S
